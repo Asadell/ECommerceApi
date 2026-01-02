2bced13b5f2f1589bb185f4dcdb94ce6410868a0
Asadell <m.satrioasadell@gmail.com>
2026-01-02 21:05:13 +0700

    chore: product migrations

--- obj/Debug/net9.0/ECommerceApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ECommerceApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa541e797e62f58c4b9888164e3523d1dce957d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0496b3c3c7d75ba30e87c35bb6c155c5102bb29")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
