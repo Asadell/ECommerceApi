3acbc08d17a3023a0002cdffcd3c1510fab8ac51
Asadell <m.satrioasadell@gmail.com>
2026-01-03 11:43:16 +0700

    chore: user migration

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,6 +10,7 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -26,6 +27,18 @@ public class AppDbContext : DbContext
             entity.Property(e => e.IsActive).IsRequired().HasDefaultValue(true);
             entity.Property(e => e.CreatedAt).IsRequired().HasDefaultValueSql("NOW()");
             entity.Property(e => e.UpdatedAt).IsRequired(false);
+        });
+
+        modelBuilder.Entity<User> (entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.PasswordHash).IsRequired();
+            entity.Property(e => e.FullName).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Role).IsRequired().HasMaxLength(20).HasDefaultValue("Customer");
+            entity.Property(e => e.CreatedAt).IsRequired().HasDefaultValueSql("NOW()");
+            entity.Property(e => e.LastLoginAt).IsRequired(false);
         });
     }
 }
--- obj/Debug/net9.0/ECommerceApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ECommerceApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bced13b5f2f1589bb185f4dcdb94ce6410868a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b0c92d12bad0de83bc24a4471cf0715e55f78a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
